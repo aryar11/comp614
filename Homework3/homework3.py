00c8045673df8c4bf1644bd589b0eb9973aeca83
aryar <aryarahmanian23@gmail.com>
2024-10-17 19:04:13 -0500

    started hw 3

---
+++ Homework3/homework3.py
@@ -0,0 +1,120 @@
+"""
+COMP 614
+Homework 3: Stock Prediction
+"""
+
+import comp614_module3 as stocks
+import random
+
+
+def markov_chain(data, order):
+    """
+    Creates and returns a Markov chain with the given order from the given data.
+
+    inputs:
+        - data: a list of ints or floats representing previously collected data
+        - order: an integer repesenting the desired order of the Markov chain
+
+    returns: a dictionary that represents the Markov chain
+    """
+    return {}
+
+
+def predict(model, last, num):
+    """
+    Predicts the next num values given the model and the last values.
+
+    inputs:
+        - model: a dictionary representing a Markov chain
+        - last: a list (with length of the order of the Markov chain)
+                representing the previous states
+        - num: an integer representing the number of desired future states
+
+    returns: a list of integers that are the next num states
+    """
+    return []
+
+
+def mse(result, expected):
+    """
+    Calculates the mean squared error between two data sets. Assumes that the 
+    two data sets have the same length.
+    
+    inputs:
+        - result: a list of integers or floats representing the actual output
+        - expected: a list of integers or floats representing the predicted output
+
+    returns: a float that is the mean squared error between the two data sets
+    """
+    return 0.0
+
+
+def run_experiment(train, order, test, future, actual, trials):
+    """
+    Runs an experiment to predict the future of the test data based on the
+    given training data.
+
+    inputs:
+        - train: a list of integers representing past stock price data
+        - order: an integer representing the order of the markov chain
+                 that will be used
+        - test: a list of integers of length "order" representing past
+                stock price data (different time period than "train")
+        - future: an integer representing the number of future days to
+                  predict
+        - actual: a list representing the actual results for the next
+                  "future" days
+        - trials: an integer representing the number of trials to run
+
+    returns: a float that is the mean squared error over the number of trials
+    """
+    return 0.0
+
+
+def run():
+    """
+    Runs the stock prediction application. You should not modify this function!
+    """
+    # Get the supported stock symbols
+    symbols = stocks.get_supported_symbols()
+
+    # Load the training data
+    changes = {}
+    bins = {}
+    for symbol in symbols:
+        prices = stocks.get_historical_prices(symbol)
+        changes[symbol] = stocks.compute_daily_change(prices)
+        bins[symbol] = stocks.bin_daily_changes(changes[symbol])
+
+    # Load the test data
+    testchanges = {}
+    testbins = {}
+    for symbol in symbols:
+        testprices = stocks.get_test_prices(symbol)
+        testchanges[symbol] = stocks.compute_daily_change(testprices)
+        testbins[symbol] = stocks.bin_daily_changes(testchanges[symbol])
+
+    # Display data
+    stocks.plot_daily_change(changes)
+    stocks.plot_bin_histogram(bins)
+
+    # Run experiments
+    orders = [1, 3, 5, 7, 9]
+    ntrials = 500
+    days = 5
+
+    for symbol in symbols:
+        print(symbol)
+        print("====")
+        print("Actual:", testbins[symbol][-days:])
+        for order in orders:
+            error = run_experiment(bins[symbol], order,
+                                   testbins[symbol][-order-days:-days], days,
+                                   testbins[symbol][-days:], ntrials)
+            print("Order", order, ":", error)
+        print()
+
+        
+# You may want to keep this call commented out while you're writing & testing
+# your code. Uncomment it when you're ready to run the experiments.
+# run()
